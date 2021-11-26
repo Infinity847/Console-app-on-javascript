668933c6957fd8c6aa042fbe99dfe9bfe8b9de28
Infinity847 <88909782+Infinity847@users.noreply.github.com>
2021-11-25 17:55:18 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+var name1 = "Timothy";
+var name2 = "Gribov";
+console.log(name1);
+console.log(name2);
+console.log(name1 + " " + name2);
+var a = "9";
+var b = "6";
+console.log(a);
+console.log(b);
+var result = (math.max(a, b))
